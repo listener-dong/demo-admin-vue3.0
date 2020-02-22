4a28e06bce220a6e42dd1e928d6a2b1e421b6d5d
chen_mbp <839823170@qq.com>
2020-02-22 22:16:50 +0800

    创建路由&引入element-ui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    登录页
+    <el-button type="primary" size="small">主要按钮</el-button>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+</style>
